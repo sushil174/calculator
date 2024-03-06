b13775cb3f14c6a113b435a775bc1200d48e7cb0
sushil174 <sushilkoregave123@gmail.com>
2024-03-06 03:23:56 -0500

    Add features

--- script.js
+++ script.js
@@ -36,41 +36,59 @@ function operate(f,s,o){
 
 }
 const equal = document.querySelector(".equal")
-const first = document.querySelector(".f")
-const op = document.querySelector(".o")
-const second = document.querySelector(".s")
 
 const buttons = Array.from(document.querySelectorAll(".button"))
 const operations = Array.from(document.querySelectorAll(".op"))
 var display = document.querySelector(".display")
 var temp = 0
+var count = 0
+var query = ""
 
 buttons.forEach((btn)=>{
     btn.addEventListener('click', () => {
         if(temp==0){
             firstNumber = firstNumber.concat(btn.value)
-            first.textContent = firstNumber
+            query = query.concat(btn.value)
         }
         else{
             secondNumber = secondNumber.concat(btn.value)
-            second.textContent = secondNumber
+            query = query.concat(btn.value)
+            temp = 0
         }
+        display.textContent = query
     })
 })
 
-operations.forEach((operate)=>{
-    operate.addEventListener('click',()=>{
-        operator = operate.value
-        op.textContent = operator
-        temp = 1
+operations.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        if(count == 0){
+            operator = btn.value
+            query = query.concat(btn.value)
+            display.textContent = query
+            temp = 1
+            count = 1
+
+        }
+        else {
+            result = operate(parseInt(firstNumber),parseInt(secondNumber),operator)
+            firstNumber = result
+            query = firstNumber.toString()
+            display.textContent = firstNumber
+            secondNumber = ""
+            operator = btn.value
+            query = query.concat(operator)
+            display.textContent = query
+            temp = 1
+        }
     })
 })
 
 equal.addEventListener('click', ()=>{
     temp = 0
     result = operate(parseInt(firstNumber),parseInt(secondNumber),operator)
-    display.textContent = result
-    firstNumber = ""
+    firstNumber = result
+    query = firstNumber.toString()
+    display.textContent = firstNumber
     secondNumber = ""
     operator = ""
 })
